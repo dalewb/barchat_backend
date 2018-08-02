18f8e8b50292f36c8580808326c1304983aa06ad
William Dale <williambdale@gmail.com>
2018-08-02 15:09:40 -0400

    Create relationships between models

--- app/controllers/app/v1/users_controller.rb
+++ app/controllers/app/v1/users_controller.rb
@@ -1,2 +1,43 @@
 class UsersController < ApplicationController
+
+  def index
+    users = User.all
+    render json: users, include: [:commodity, :user]
+  end
+
+  def show
+    user = User.find(params[:id])
+    render json: {status: 'SUCCESS', user: 'Loaded User', data: user}, status: :ok
+  end
+
+  def create
+    user = User.new(user_params)
+    if user.save
+      render json: {status: 'SUCCESS', user: 'Loaded User', data: user}, status: :ok
+    else
+      render json: {status: 'ERROR', user: 'User Not Saved', data: user.errors}, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    user = User.find(params[:id])
+    if user.update_attributes(user_params)
+      render json: {status: 'SUCCESS', user: 'Updated User', data: user}, status: :ok
+    else
+      render json: {status: 'ERROR', user: 'User Not Updated', data: user.errors}, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    user = User.find(params[:id])
+    user.destroy
+    render json: {status: 'SUCCESS', user: 'Deleted User', data: user}, status: :ok
+  end
+
+  private
+
+  def user_params
+    params.permit(:description, :viewable, :location_id, :age, :gender)
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  belongs_to :location
+  has_many :permissions
+  has_many :messages 
 end
